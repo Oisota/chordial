0d0e85d1f94247f529f52589d8c4a3d9dd3dde56
Derek Morey <derek.o.morey@gmail.com>
2022-06-20 22:32:30 -0400

    Added basic search functionality

--- src/components/SongList.vue
+++ src/components/SongList.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref, onBeforeMount } from 'vue';
+import { ref, onBeforeMount, computed } from 'vue';
 import { useRouter } from 'vue-router';
 import { Pencil, Plus, Trash } from 'lucide-vue-next';
 import { v4 as uuid4 } from 'uuid';
@@ -8,6 +8,10 @@ import { useSongsStore } from '../stores/all-songs';
 import { Song } from '../types';
 import { PouchDocType } from '../db';
 
+const props = defineProps<{
+	searchText: string
+}>();
+
 const router = useRouter();
 const songs = useSongsStore();
 const newSongInput = ref(null);
@@ -15,6 +19,17 @@ const songName = ref('');
 
 onBeforeMount(async () => {
 	return songs.load();
+});
+
+const filteredSongs = computed(() => {
+	if (!props.searchText) {
+		return songs.songs;
+	}
+	return songs.songs.filter((s: Song) => {
+		const titleMatch = s.title.includes(props.searchText);
+		const artistMatch = s.artist.includes(props.searchText);
+		return titleMatch || artistMatch;
+	});
 });
 
 function add() {
@@ -34,7 +49,11 @@ function add() {
 function remove(song: Song) {
 	songs.remove(song);
 }
-function viewSong(song: Song) {
+function viewSong(song: Song, event: Event) {
+	if (event.target && !event.target.classList.contains('list-group-item-action')) {
+		return;
+	}
+	// check if other buttons were clicked and stop navigation so other buttons can be handled
 	router.push({
 		name: 'view',
 		params: {
@@ -52,8 +71,12 @@ function viewSong(song: Song) {
 				<Plus size="16"/>
 			</button>
 		</div>
-		<button v-for="song in songs.songs" type="button" v-on:click="viewSong(song)" class="list-group-item list-group-item-action d-flex flex-row justify-content-between align-items-center">
-			<span>{{song.title}}</span>
+		<button v-for="song in filteredSongs" type="button" v-on:click="viewSong(song, $event)" class="list-group-item list-group-item-action d-flex flex-row justify-content-between align-items-center">
+			<span>
+				{{song.title}}
+				<small class="text-secondary ms-2">{{song.artist}}</small>
+			</span>
+			
 			<div class="btn-group">
 				<button type="button" class="btn btn-danger" v-on:click="remove(song)">
 					<Trash size="15"></Trash>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,11 +1,16 @@
 <script setup lang="ts">
+import { ref} from 'vue';
+
 import SongList from '../components/SongList.vue';
+
+const searchText = ref('');
 </script>
 
 <template>
 	<div class="row mt-2">
 		<div class="col-xl-6 offset-xl-3">
-			<SongList/>
+			<input v-model="searchText" type="text" class="form-control mb-2" placeholder="Search Songs">
+			<SongList :searchText="searchText"/>
 		</div>
 	</div>
 </template>
